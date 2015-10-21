8e7ff47b65f833fda64efe03bb1f52bee3c3fa61
Julia Gao <juliagao2005@gmail.com>
2015-10-21 10:24:48 -0600

    added more images and adjusted slides

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -10,39 +10,73 @@ import preloader from "../src/utils/preloader";
 import Interactive from "./interactive";
 
 const images = {
-  city: require("./city.jpg"),
-  kat: require("./kat.png"),
-  logo: require("./formidable-logo.svg")
+  bigbang: require('./bigbang.gif'),
+  clap: require('./clap.gif'),
+  clojure_atom: require('./clojure_atom.png'),
+  comfort_zone: require('./comfort_zone.png'),
+  console_minus: require('./console_minus.png'),
+  console_plus: require('./console_plus.png'),
+  does_not_simply: require('./does_not_simply.png'),
+  expand_comfort: require('./expand_comfort.png'),
+  false: require('./false.png'),
+  fp: require('./fp.png'),
+  haskell: require('./haskell.png'),
+  inefficient: require('./inefficient.gif'),
+  lol: require('./lol.png'),
+  make_better: require('./make_better.png'),
+  make_const: require('./make_const.png'),
+  make_let: require('./make_let.png'),
+  mario8: require('./mario8.jpg'),
+  nan: require('./nan.png'),
+  not_working: require('./not_working.gif'),
+  nullJS: require('./null.png'),
+  oo_fp_patterns: require('./oo_fp_patterns.png'),
+  purescript: require('./purscript.png'),
+  reactjs: require('./reactjs.png'),
+  tries: require('./tries.png'),
+  typeof_nan: require('./typeof_nan.png')
 };
 
-preloader([images.city, images.kat]);
+//preloader([images.city, images.kat]);
 
 export default class extends React.Component {
   render() {
     return (
       <Deck transition={["zoom", "slide"]} transitionDuration={800}>
-        <Slide transition={["zoom"]} bgColor="primary">
+        <Slide transition={["zoom"]} bgColor="white">
           <Heading size={1} fit caps textColor="black">
             Front-end can be more functional
           </Heading>
           <Text>- Julia Gao</Text>
         </Slide>
-        <Slide transition={['slide']} bgColor="white">
-          <Text>
-            github.com/ryoia/reactive-conf-slides
-          </Text>
+        <Slide transition={['slide']} bgColor="white"
+          notes="Who am i...I like astronomy and reactjs and functional programming
+          and video games, so if you are interested in any of those things, I'd like to chat with you">
+          <Heading size={2} textColor="black">
+            Who Am I?
+          </Heading>
+          <Image height="20%" width="30%" margin="10" src={images.bigbang}/>
+          <Image height="20%" width="30%" margin="10" src={images.reactjs}/>
+          <Image height="20%" width="30%" margin="10" src={images.fp}/>
+          <Image height="20%" width="30%" margin="10" src={images.lol}/>
+          <Image height="20%" width="30%" margin="10" src={images.mario8}/>
+        </Slide>
+        <Slide>
         </Slide>
         <Slide transition={['slide']} bgColor="white" notes="So I'm gonna do a super quick introduction on functional programming before we dive into how to make frontend more functional">
-          <Appear fid="1">
-            <Heading size={1} caps fit textColor="black">
-              Two Main Concepts on Functional Programming
-            </Heading>
-          </Appear>
-          <Appear fid="2">
-            <Heading size={1} caps fit textColor="black">
-              How to Make Front-End More Functional Using the Tools We Currently Have
-            </Heading>
-          </Appear>
+          <List>
+            <ListItem><Appear fid="1">
+              Main Conepts on Functional Programming
+            </Appear></ListItem>
+            <ListItem><Appear fid="2">
+              How to make our front-end applications more functional using the tools we currently have
+            </Appear></ListItem>
+          </List>
+        </Slide>
+        <Slide transition={['slide']} bgColor="white"
+          notes="So basically, a function call either returns a value, or another function call.
+          Now, let's see two concepts that hopefully make the functional-style make more sense">
+          <Image width="100%" height="100%" src={images.oo_fp_patterns} />
         </Slide>
         <Slide transition={['slide']} bgColor="primary">
           <Heading>
@@ -60,22 +94,23 @@ export default class extends React.Component {
           </Text>
         </Slide>
         <Slide>
-          <Appear>
-            <CodePane
-              lang="javascript"
-              source={require("raw!./abs.example")}
-              margin="20px auto"/>
-          </Appear>
-          <Appear>
+          <CodePane
+            lang="javascript"
+            source={require("raw!./abs.example")}
+            margin="20px auto"/>
+          <Appear fid="1">
             <Heading>
               Advantages?
             </Heading>
-            <Text>
-              - No side effects
-              - Function acts the same whenever you call it
-              - Fewer bugs
-            </Text>
+            <List>
+              <ListItem><Appear fid="2"> No side effects</Appear></ListItem>
+              <ListItem><Appear fid="3"> Function acts the same whenever you call it</Appear></ListItem>
+              <ListItem><Appear fid="4"> Fewer bugs</Appear></ListItem>
+            </List>
           </Appear>
+        </Slide>
+        <Slide transition={["slide"]} bgDarken={0.75}>
+          <Image width="100%" src={images.inefficient}/>
         </Slide>
         <Slide transition={['zoom', 'fade']} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
           <CodePane
@@ -83,7 +118,7 @@ export default class extends React.Component {
             source={require("raw!./deck.example")}
             margin="20px auto"/>
         </Slide>
-        <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
+        <Slide transition={["slide"]} bgDarken={0.75}>
           <Appear fid="1">
             <Heading size={1} caps fit textColor="primary">
               Full Width
@@ -149,7 +184,12 @@ export default class extends React.Component {
           <Heading size={1} caps fit textColor="primary">
             Made with love in Seattle by
           </Heading>
-          <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.logo}/></Link>
+          <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.lol}/></Link>
+        </Slide>
+        <Slide transition={['slide']} bgColor="white">
+          <Text>
+            github.com/ryoia/reactive-conf-slides
+          </Text>
         </Slide>
       </Deck>
     );
