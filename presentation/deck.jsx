bdde43a082147614cb36b1605811f9322525510c
Julia Gao <juliagao2005@gmail.com>
2015-10-25 11:18:56 -0600

    more changes on slides

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -73,17 +73,16 @@ export default class extends React.Component {
           </List>
         </Slide>
         <Slide transition={['slide']} bgColor="white"
-          notes="So basically, a function call either returns a value, or another function call.
-          Now, let's see two concepts that hopefully make the functional-style make more sense">
+          notes="So basically, a function call either returns a value, or another function call. Now, let's see two concepts that hopefully make the functional-style make more sense">
           <Image width="100%" height="100%" src={images.oo_fp_patterns} />
         </Slide>
-        <Slide transition={['slide']} bgColor="primary">
+        <Slide transition={['slide']} bgColor="white">
           <Heading>
             Stateless & Immutable
           </Heading>
         </Slide>
-        <Slide>
-          <Heading>
+        <Slide bgColor="white">
+          <Heading size={2} textColor="black">
             Stateless (Idempotence)
           </Heading>
           <Text>
@@ -92,7 +91,7 @@ export default class extends React.Component {
             the output should be the same with the same parameters
           </Text>
         </Slide>
-        <Slide>
+        <Slide bgColor="white">
           <CodePane
             lang="javascript"
             source={require("raw!./abs.example")}
@@ -108,32 +107,30 @@ export default class extends React.Component {
             </List>
           </Appear>
         </Slide>
-        <Slide notes="Can be due to somebody else changed something somewhere, we don't want that to happen">
+        <Slide bgColor="white" notes="Can be due to somebody else changed something somewhere, we don't want that to happen">
           <Text>When a thing that worked on Friday no longer works on Monday</Text>
           <Image width="100%" height="100%" src={images.not_working} />
         </Slide>
-        <Slide
-          notes="The doStuff function here has side effect because the result depends on what the globalVariable is, and globalVariable can be changed by doOtherStuff function. This isn’t what we want because now the outputs are not the same when we pass in the same arguments every time.
-          ">
+        <Slide bgColor="white"
+          notes="The doStuff function here has side effect because the result depends on what the globalVariable is, and globalVariable can be changed by doOtherStuff function. This isn’t what we want because now the outputs are not the same when we pass in the same arguments every time.">
           <CodePane
             lang="javascript"
             size
             source={require("raw!./side_effect.example")}
             margin="20px auto"/>
         </Slide>
-        <Slide>
+        <Slide bgColor="white">
           <CodePane
             lang="javascript"
             size
             source={require("raw!./less_side_effect.example")}
             margin="20px auto"/>
         </Slide>
-        <Slide>
+        <Slide bgColor="white">
           <Appear><Heading size={6}>Immutable Data Structures</Heading></Appear>
           <Appear><Image height="500px" src={images.does_not_simply} /></Appear>
         </Slide>
-        <Slide notes="Why immutability is crucial. Think about the function we have, we pass in some parameters, but if they are mutable, we may do something within the function that changes the passed in parameter, so that doesn’t guarantee our results anymore. What would happen if you have a mutable data structure that will be changed in two different threads, the result wouldn’t be consistent and the change being made in the slow thread can be overriden. 
-        ">
+        <Slide bgColor="white" notes="Why immutability is crucial. Think about the function we have, we pass in some parameters, but if they are mutable, we may do something within the function that changes the passed in parameter, so that doesn’t guarantee our results anymore. What would happen if you have a mutable data structure that will be changed in two different threads, the result wouldn’t be consistent and the change being made in the slow thread can be overriden. ">
           <Heading size={6}>ImmutableDataStructures</Heading>
           <Text>Once assigned, can't be mutated. e.g. 5, "hi"</Text>
           <Appear>
@@ -147,7 +144,7 @@ export default class extends React.Component {
               margin="20px auto"/>
           </Appear>
         </Slide>
-        <Slide>
+        <Slide bgColor="white">
           <Text>How to change this function to use immutable data structures?</Text>
             <CodePane
               lang="javascript"
@@ -156,22 +153,22 @@ export default class extends React.Component {
               margin="20px auto"/>
           <Text>Recursion!</Text>
         </Slide>
-        <Slide>
+        <Slide bgColor="white">
             <CodePane
               lang="javascript"
               size
               source={require("raw!./findMaxRecursion.example")}
               margin="20px auto"/>
         </Slide>
-        <Slide>
-          <Heading size={1} fit caps>How to Make Frontend More Functional</Heading>
+        <Slide bgColor="white">
+          <Heading size={1} textColor="black" fit caps>How to Make Frontend More Functional</Heading>
         </Slide>
-        <Slide>
-          <Heading size={1} fit caps>Disclaimer</Heading>
+        <Slide bgColor="white">
+          <Heading size={1} textColor="black" fit caps>Disclaimer</Heading>
         </Slide>
-        <Slide>
+        <Slide bgColor="white">
           <Appear>
-            <Image width="20%" src={images.nullJS} />
+            <Image margin="0px auto 40px" width="20%" src={images.nullJS} />
           </Appear>
           <Appear>
             <Image width="20%" src={images.nan} />
@@ -189,32 +186,30 @@ export default class extends React.Component {
             <Image width="20%" src={images.typeof_nan} />
           </Appear>
         </Slide>
-        <Slide>
-          <Image src={images.make_better} />
+        <Slide bgColor="white">
+          <Image width="100%" src={images.make_better} />
         </Slide>
-        <Slide>
-          <Image src={images.make_let} />
+        <Slide bgColor="white">
+          <Image width="80%" height="600px" src={images.make_let} />
         </Slide>
-        <Slide notes="let and const are block scoped, no more hoisting. const wouldn’t let you change the value, however, like what I said about the disclaimer, this is still javascript, which means some values can be changed.
-        ">
+        <Slide bgColor="white" notes="let and const are block scoped, no more hoisting. const wouldn’t let you change the value, however, like what I said about the disclaimer, this is still javascript, which means some values can be changed.">
           <Image src={images.make_const} />
         </Slide>
-        <Slide notes="let and const are block scoped, no more hoisting. let variables can still be mutated. ">
+        <Slide bgColor="white" notes="let and const are block scoped, no more hoisting. let variables can still be mutated. ">
           <CodePane
             lang="javascript"
             size
             source={require("raw!./let.example")}
             margin="20px auto"/>
         </Slide>
-        <Slide notes="const variables cannot be changed once assigned, they have to be initialized when declared.
-        ">
+        <Slide bgColor="white" notes="const variables cannot be changed once assigned, they have to be initialized when declared.">
           <CodePane
             lang="javascript"
             size
             source={require("raw!./const.example")}
             margin="20px auto"/>
         </Slide>
-        <Slide>
+        <Slide bgColor="white">
           <Heading size={6}>Libraries/Frameworks that can make front-end more functional</Heading>
           <List>
             <ListItem>ImmutableJS with ReactJS</ListItem>
@@ -223,26 +218,24 @@ export default class extends React.Component {
             <ListItem>What do you use?</ListItem>
           </List>
         </Slide>
-        <Slide notes="This already looks promising since immutability is one of the cores FP has. ImmutableJS provides immutable data structures, so like map, set, list are immutable, which means, you make a data structure every time there is a change.
-        ">
-          <Heading size={1} caps>ImmutableJS</Heading>
+        <Slide bgColor="white" notes="This already looks promising since immutability is one of the cores FP has. ImmutableJS provides immutable data structures, so like map, set, list are immutable, which means, you make a data structure every time there is a change.">
+          <Heading size={1} textColor="black" caps>ImmutableJS</Heading>
         </Slide>
-        <Slide notes="put copies of powerpoints here. explaining I wanted to make my preparing process immutable as well. so now you may wonder, since I’m making a copy of every time I make a change, this is super heavy and slow. but, immutablejs actually has this persistent data structures, which means they make a copy of the changed items, then link it back to the unchanged things. this is a simplified version of course, if you want to learn more specifics, do look up tries implementation that immutablejs uses. 
-        ">
-          <Image src={images.copies} />
+        <Slide bgColor="white" notes="put copies of powerpoints here. explaining I wanted to make my preparing process immutable as well. so now you may wonder, since I’m making a copy of every time I make a change, this is super heavy and slow. but, immutablejs actually has this persistent data structures, which means they make a copy of the changed items, then link it back to the unchanged things. this is a simplified version of course, if you want to learn more specifics, do look up tries implementation that immutablejs uses. ">
+          <Image width="100%" src={images.copies} />
         </Slide>
-        <Slide>
+        <Slide bgColor="white">
           <Heading size={6}>But that's so inefficient!</Heading>
           <Image src={images.inefficient} />
         </Slide>
-        <Slide notes="immutablejs uses trie so only copy the changed items instead of the entire list">
+        <Slide bgColor="white" notes="immutablejs uses trie so only copy the changed items instead of the entire list">
           <Text>Persistent Data Structure HashMap Tries, Vector Tries</Text>
-          <Image src={images.tries} />
+          <Image width="100%" src={images.tries} />
         </Slide>
-        <Slide notes="When I first read this, I was like, this is genius">
+        <Slide bgColor="white" notes="When I first read this, I was like, this is genius">
           <Image src={images.clap} />
         </Slide>
-        <Slide notes="Here's a comparison between regular js and immutablejs">
+        <Slide bgColor="white" notes="Here's a comparison between regular js and immutablejs">
           <Layout>
             <Fill>
               <CodePane
@@ -260,7 +253,7 @@ export default class extends React.Component {
             </Fill>
           </Layout>
         </Slide>
-        <Slide notes="so let's use immutablejs">
+        <Slide bgColor="white" notes="so let's use immutablejs">
           <Text>Here's another example</Text>
           <Appear>
             <CodePane
@@ -270,15 +263,15 @@ export default class extends React.Component {
               margin="20px auto"/>
           </Appear>
         </Slide>
-        <Slide notes="Clojure is a dialect of Lisp, and clojurescript is a dialect of Clojure that compiles to javascript. Clojurescript also has immutable data structures, and since it’s a dialect of Lisp, it has Lisp syntax.">
-          <Heading size={1} cap>clojurescript</Heading>
+        <Slide bgColor="white" notes="Clojure is a dialect of Lisp, and clojurescript is a dialect of Clojure that compiles to javascript. Clojurescript also has immutable data structures, and since it’s a dialect of Lisp, it has Lisp syntax.">
+          <Heading size={1} textColor="black" cap>clojurescript</Heading>
           <CodePane
             lang="javascript"
             size
             source={require("raw!./clojs.example")}
             margin="20px auto"/>
         </Slide>
-        <Slide>
+        <Slide bgColor="white">
           <Text>Modular:</Text>
           <Code>(ns myapp ...</Code>
           <Code>(ns hello_world.core ...</Code>
@@ -289,7 +282,7 @@ export default class extends React.Component {
              v // [1 2 3]
           </Code>
         </Slide>
-        <Slide notes="Reagent, can write react components in clojurescript">
+        <Slide bgColor="white" notes="Reagent, can write react components in clojurescript">
           <Heading size={2} cap>reagent</Heading>
           <CodePane
             lang="javascript"
@@ -302,119 +295,46 @@ export default class extends React.Component {
             source={require("raw!./reagent2.example")}
             margin="20px auto"/>
         </Slide>
-        <Slide notes="the intended use of atom is to hold one of Clojure's immutable data structures. And, similar to ref's alter and agent's send, you change the value by applying a function to the old value. This is done in an atomic manner by swap! Internally, swap! reads the current value, applies the function to it, and attempts to compare-and-set it in. Since another thread may have changed the value in the intervening time, it may have to retry, and does so in a spin loop. ">
+        <Slide bgColor="white" notes="the intended use of atom is to hold one of Clojure's immutable data structures. And, similar to ref's alter and agent's send, you change the value by applying a function to the old value. This is done in an atomic manner by swap! Internally, swap! reads the current value, applies the function to it, and attempts to compare-and-set it in. Since another thread may have changed the value in the intervening time, it may have to retry, and does so in a spin loop. ">
           <Image src={images.clojure_atom} /> 
         </Slide>
-        <Slide notes="Reagent has its own version of atom, which re-renders itself whenever it’s changed by tracking when it is deref’ed. sounds familiar??">
+        <Slide bgColor="white" notes="Reagent has its own version of atom, which re-renders itself whenever it’s changed by tracking when it is deref’ed. sounds familiar??">
           <CodePane
             lang="javascript"
             size
             source={require("raw!./reagent_atom.example")}
             margin="20px auto"/>
         </Slide>
-        <Slide notes="written in Haskell, strongly typed language that compiles to javascript. Hence, everything else is impurescript.">
+        <Slide bgColor="white" notes="written in Haskell, strongly typed language that compiles to javascript. Hence, everything else is impurescript.">
           <Heading cap size={2}>Purescript</Heading>
-          <Image src={images.haskell} />
+          <Image width="100%" src={images.haskell} />
         </Slide>
-        <Slide>
+        <Slide bgColor="white">
           <CodePane
             lang="javascript"
             size
             source={require("raw!./purs.example")}
             margin="20px auto"/>
         </Slide>
-        <Slide>
-          <Image src={images.comfort_zone} />
+        <Slide bgColor="white">
+          <Image width="100%" src={images.comfort_zone} />
         </Slide>
-        <Slide>
-          <Image src={images.expand_comfort} />
+        <Slide bgColor="white">
+          <Image width="100%" src={images.expand_comfort} />
         </Slide>
-        <Slide>
-          <Text>Thank you!</Text>
-          <Text>Twitter: @ryoia</Text>
+        <Slide bgColor="white" transition={['slide']} bgColor="white"
+          notes="this is all i have, so if you wanna chat with any of these, or other interesting sciency things">
+          <Image height="20%" width="30%" margin="10" src={images.bigbang}/>
+          <Image height="20%" width="30%" margin="10" src={images.reactjs}/>
+          <Image height="20%" width="30%" margin="10" src={images.fp}/>
+          <Image height="20%" width="30%" margin="10" src={images.lol}/>
+          <Image height="20%" width="30%" margin="10" src={images.mario8}/>
         </Slide>
-
-        <Slide transition={["slide"]} bgDarken={0.75}>
-          <Image width="100%" src={images.inefficient}/>
+        <Slide bgColor="white">
+          <Heading size={2}>Twitter: @ryoia</Heading>
         </Slide>
-        <Slide transition={['zoom', 'fade']} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
-          <CodePane
-            lang="javascript"
-            source={require("raw!./deck.example")}
-            margin="20px auto"/>
-        </Slide>
-        <Slide transition={["slide"]} bgDarken={0.75}>
-          <Appear fid="1">
-            <Heading size={1} caps fit textColor="primary">
-              Full Width
-            </Heading>
-          </Appear>
-          <Appear fid="2">
-            <Heading size={1} caps fit textColor="tertiary">
-              Adjustable Darkness
-            </Heading>
-          </Appear>
-          <Appear fid="3">
-            <Heading size={1} caps fit textColor="primary">
-              Background Imagery
-            </Heading>
-          </Appear>
-        </Slide>
-        <Slide transition={["zoom", "fade"]} bgColor="primary">
-          <Heading caps fit>Flexible Layouts</Heading>
-          <Layout>
-            <Fill>
-              <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                Left
-              </Heading>
-            </Fill>
-            <Fill>
-              <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                Right
-              </Heading>
-            </Fill>
-          </Layout>
-        </Slide>
-        <Slide transition={["slide"]} bgColor="black">
-          <BlockQuote>
-            <Quote>Wonderfully formatted quotes</Quote>
-            <Cite>Ken Wheeler</Cite>
-          </BlockQuote>
-        </Slide>
-        <Slide transition={["slide", "spin"]} bgColor="primary">
-          <Heading caps fit size={1} textColor="tertiary">
-            Smooth
-          </Heading>
-          <Heading caps fit size={1} textColor="secondary">
-            Combinable Transitions
-          </Heading>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <List>
-            <ListItem><Appear fid="1">Inline style based theme system</Appear></ListItem>
-            <ListItem><Appear fid="2">Autofit text</Appear></ListItem>
-            <ListItem><Appear fid="3">Flexbox layout system</Appear></ListItem>
-            <ListItem><Appear fid="4">React-Router navigation</Appear></ListItem>
-            <ListItem><Appear fid="5">PDF export</Appear></ListItem>
-            <ListItem><Appear fid="6">And...</Appear></ListItem>
-          </List>
-        </Slide>
-        <Slide transition={["slide"]} bgColor="primary">
-          <Heading size={1} caps fit textColor="tertiary">
-            Your presentations are interactive
-          </Heading>
-          <Interactive/>
-        </Slide>
-        <Slide transition={["spin", "slide"]} bgColor="tertiary">
-          <Heading size={1} caps fit textColor="primary">
-            Made with love in Seattle by
-          </Heading>
-          <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.lol}/></Link>
-        </Slide>
-        <Slide transition={['slide']} bgColor="white">
-          <Text>
-            github.com/ryoia/reactive-conf-slides
-          </Text>
+        <Slide bgColor="white">
+          <Heading size={2}>Thank you!</Heading>
         </Slide>
       </Deck>
     );
